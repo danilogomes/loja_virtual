257ba58de123d4cfb7632e42979f8fe019e8a863
Danilo Gomes <danilomlgomes@gmail.com>
2016-01-18 22:30:27 -0300

    inserindo metodo to_s melhorando a imperssão do objeto

--- lib/livro.rb
+++ lib/livro.rb
@@ -5,4 +5,8 @@ class Livro
           @isbn = isbn
           @numero_paginas = numero_de_paginas      
 	end
+
+        def to_s
+          "Autor: #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}"
+        end
 end
