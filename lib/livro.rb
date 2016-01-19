e1ac36d62c3dcd297c3e592af6a9a32c6bbb8f80
Danilo Gomes <danilomlgomes@gmail.com>
2016-01-18 22:13:35 -0300

    adicionando variáveis de instância

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,5 +1,8 @@
 class Livro
-	def initializer(autor, isbn = "1", numero_de_paginas)
-		puts "Autor: #{autor}, ISBN: #{isbn}, Pág: #{numero_de_paginas}"
+	def initialize(autor, isbn = "1", numero_de_paginas)
+	  #puts "Autor: #{autor}, ISBN: #{isbn}, Pág: #{numero_de_paginas}"
+          @autor = autor
+          @isbn = isbn
+          @numero_paginas = numero_de_paginas      
 	end
 end
