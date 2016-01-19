661058aeb6c80a55e8140100417681341a4712a2
Danilo Gomes <danilomlgomes@gmail.com>
2016-01-19 13:10:23 -0300

    Adicionando metodo preço para manipular o valor

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,17 +1,21 @@
 class Livro
 	def initialize(autor, isbn = "1", numero_de_paginas, preco)
 	  #puts "Autor: #{autor}, ISBN: #{isbn}, Pág: #{numero_de_paginas}"
-          @autor = autor
-          @isbn = isbn
-          @numero_paginas = numero_de_paginas
-          @preco = preco      
+    @autor = autor
+    @isbn = isbn
+    @numero_paginas = numero_de_paginas
+    @preco = preco      
 	end
 
-        def to_s
-          "Autor: #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}"
-        end
+  def to_s
+    "Autor: #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}"
+  end
 
-        def preco
-          @preco
-        end
+  def preco
+    @preco
+  end
+
+  def preco=(preco)
+    @preco = preco
+  end
 end
