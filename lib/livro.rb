88a41a3919b5f638e05496caf054543f2fdf4fb5
Danilo Gomes <danilomlgomes@gmail.com>
2016-01-19 22:31:47 -0300

    adicionando categoria a livro

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,26 +1,28 @@
 class Livro
 
-  attr_accessor :preco # funciona como o attr_writer e o attr_reader juntos
+  attr_accessor :valor # funciona como o attr_writer e o attr_reader juntos
+  attr_reader :categoria
   #attr_writer :preco
   #attr_reader :preco
 
-	def initialize(autor, isbn = "1", numero_de_paginas, preco)
+	def initialize(autor, isbn = "1", numero_de_paginas, valor, categoria)
 	  #puts "Autor: #{autor}, ISBN: #{isbn}, Pág: #{numero_de_paginas}"
     @autor = autor
     @isbn = isbn
     @numero_paginas = numero_de_paginas
-    @preco = preco      
+    @valor = valor
+    @categoria = categoria      
 	end
 
   def to_s
     "Autor: #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}"
   end
 
-  def preco
-    @preco
+  def valor
+    @valor
   end
 
-  def preco=(preco)
-    @preco = preco
+  def valor=(valor)
+    @valor = valor
   end
 end
