6264430e4ab4ed7368457b3c26f245a55bf8ad3b
Danilo Gomes <danilomlgomes@gmail.com>
2016-01-25 13:36:54 -0300

    inserindo identação correta e adicionando o método livros

--- lib/biblioteca.rb
+++ lib/biblioteca.rb
@@ -1,14 +1,18 @@
 class Biblioteca
 
-	attr_reader :livros
+  attr_reader :livros
 
-	def initialize
-		@livros = {}
-	end
+  def initialize
+    @livros = {}
+  end
+  
+  def adiciona(livro)
+    @livros[livro.categoria] ||= []
+    @livros[livro.categoria] << livro
+    # @livros << livro
+  end
 
-	def adiciona(livro)
-		@livros[livro.categoria] ||= []
-		@livros[livro.categoria] << livro
-		# @livros << livro
-	end
+  def livros
+    @livros.values.flatten
+  end
 end
--- lib/livro.rb
+++ lib/livro.rb
@@ -5,14 +5,14 @@ class Livro
   #attr_writer :preco
   #attr_reader :preco
 
-	def initialize(autor, isbn = "1", numero_de_paginas, valor, categoria)
-	  #puts "Autor: #{autor}, ISBN: #{isbn}, Pág: #{numero_de_paginas}"
+  def initialize(autor, isbn = "1", numero_de_paginas, valor, categoria)
+    #puts "Autor: #{autor}, ISBN: #{isbn}, Pág: #{numero_de_paginas}"
     @autor = autor
     @isbn = isbn
     @numero_paginas = numero_de_paginas
     @valor = valor
     @categoria = categoria      
-	end
+  end
 
   def to_s
     "Autor: #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}"
