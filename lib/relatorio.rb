69c8ef3a0d5bf7e3e36a43234b04995147195b14
Danilo Gomes <danilomlgomes@gmail.com>
2016-02-11 23:27:20 -0300

    adjustment relatorio class

--- lib/relatorio.rb
+++ lib/relatorio.rb
@@ -4,10 +4,6 @@ class Relatorio
   end
 
   def total
-    soma = 0.0
-    @biblioteca.livros.each do |livro|
-      soma += livro.valor
-    end
-    soma
+    @biblioteca.livros.inject(0) { |tot, livro| tot += livro.valor }
   end
 end
